0f917a021275e02d678ab13220d17d0593a4d83e
David Mazza <DavidMazza@Davids-MBP.attlocal.net>
2022-01-28 12:31:26 -0600

    CORS ADDED

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ require('dotenv').config()
 const PORT = process.env.PORT
 // const MONGODB_URI = 'mongodb://127.0.0.1:27017/chewBackEnd'
 
+// SETUP CORS middleware. Express CORS docs
+const allowedList = ['http:localhost:3000', 'your Heroku application']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(allowedList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}
+
+//CORS Middleware
+app.use(cors(corsOptions))
 
 
 //SETUP Database connection
